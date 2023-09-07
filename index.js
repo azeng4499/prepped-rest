b17073780cf69ec8796240ba7e5c9a72a9304727
Aaron Zeng <aaronz2003@gmail.com>
2023-09-07 19:41:18 -0400

    new changes

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const fs = require("fs");
 const OpenAI = require("openai");
 
 const openai = new OpenAI({
-  apiKey: "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",
+  apiKey: "",
 });
 
 app.use(express.urlencoded({ extended: true }));
@@ -26,14 +26,35 @@ app.post("/whisper", cors(), upload.single("audio-file"), async (req, res) => {
       file: fs.createReadStream(tempFilePath),
       model: "whisper-1",
     });
-    console.log(transcription.text);
     fs.unlinkSync(tempFilePath);
 
-    // res.send(response.data);
+    res.send({
+      transcription: transcription.text,
+    });
   } catch (error) {
     console.log("Error:", error.message);
     // res.status(500).send("Internal Server Error");
   }
+});
+
+const strengths = [
+  "The response draws on specific initiatives and attributes of JPMorgan Chase, showing that the candidate has done their homework. This demonstrates genuine interest and dedication.",
+  "        By highlighting diversity, innovation, and professional growth, the answer aligns the candidate's personal values with those of the company, building common ground.",
+  "           The response successfully intertwines personal aspirations with professional goals, portraying a holistic view of the candidate's motivations.",
+];
+
+const areasOfImprovement = [
+  "The answer, while thorough, can be perceived as lengthy. A more concise response might be more impactful, especially in a time-constrained interview setting.",
+  "While the answer touches on the broad tech initiatives, it doesn't delve into how the specific software engineer role fits into these initiatives or the candidate's expertise in relevant technologies.",
+  "Incorporating past quantifiable achievements or experiences that align with the candidate's motivations to work for JPMorgan Chase could add more credibility to the response.",
+];
+
+app.post("/completions", cors(), async (req, res) => {
+  console.log(req.body.transcript);
+  res.send({
+    strengths: strengths,
+    improvements: areasOfImprovement,
+  });
 });
 
 app.listen(port, () => {
